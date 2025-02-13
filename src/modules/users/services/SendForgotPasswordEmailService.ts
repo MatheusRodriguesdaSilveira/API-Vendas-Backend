b5f27272bbace39d85e19223756c1649fdfcf3c9
Matheus Rodrigues da Silveira <matheussilveira2005@gmail.com>
2025-02-13 18:14:30 -0300

    add: Message Email in SendPasswordEmail

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -3,6 +3,7 @@ import UserRepository from '../typeorm/repositories/UserRepository';
 import UserTokenRepository from '../typeorm/repositories/UsersTokensRepository';
 import { AppError } from '@shared/errors/AppError';
 import EtherealMail from '@config/mail/EtherealMail';
+import path from 'path';
 
 interface IRequest {
   email: string;
@@ -21,7 +22,12 @@ class SendForgotPasswordEmailService {
 
     const { token } = await userTokensRepository.generate(userExists.id);
 
-    // console.log(token);
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
 
     await EtherealMail.sendMail({
       to: {
@@ -30,10 +36,10 @@ class SendForgotPasswordEmailService {
       },
       subject: '[API Vendas] Recuperação de Senha',
       templateData: {
-        template: `Olá {{name}}: {{token}}`,
+        file: forgotPasswordTemplate,
         variables: {
           name: userExists.name,
-          token,
+          link: `http://localhost:3000/reset_password?token=${token}`,
         },
       },
     });
